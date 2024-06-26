a1bdfcd4f4cd98f98e2e82a1db9e89cfb829cba7
MisoFana <102695220+MisoFana@users.noreply.github.com>
2024-06-26 23:26:06 +0300

    some-changes

---
+++ README.md
@@ -0,0 +1,10 @@
+How to launch
+* .env.example > .env
+* fill .env
+* npm i
+* npx sequelize-cli init
+* npx sequelize-cli db:migrate
+* npm start
+
+
+'Alan.postman_collection.json' import into Postman for test
---
+++ migrations/20240626170312-create-product.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      photo: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.FLOAT
+      },
+      article: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
